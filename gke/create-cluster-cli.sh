118c6684b9ba7a843e09350f2c8def14b2353c1a
MatthewJDavis <matthewdavis111@gmail.com>
2020-05-23 20:03:39 -0400

    update cli

--- gke/create-cluster-cli.sh
+++ gke/create-cluster-cli.sh
@@ -4,8 +4,18 @@ gcloud config set compute/zone northamerica-northeast1-a
 gcloud container clusters create matt-cluster
 
 # get creds for the cluster
+gcloud projects list
+projid=playground-s-11-6ccaf0
 
-gcloud container clusters get-credentials matt-cluster --zone northamerica-northeast1-a --project playground-s-11-6ccaf0
+gcloud container clusters get-credentials matt-cluster --zone northamerica-northeast1-a --project $projid
 
 kubectl apply -f deployment.yml
 kubectl apply -f service.yml
+
+# update deployment then roll back
+kubectl apply -f deployment.yml
+kubectl rollout undo deployment.v1.apps/dotnet-core-deployment
+
+# cheaper
+gcloud container clusters create cheaper-cluster --num-nodes=2 --machine-type=e2-medium
+gcloud container clusters get-credentials cheaper-cluster --zone northamerica-northeast1-a --project $projid
